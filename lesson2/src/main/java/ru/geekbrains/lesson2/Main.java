b0e866fb2845c791a644d7481811220d582b0855
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-01-26 19:04:56 +0300

    Write methods for finding min/max element in array

--- lesson2/src/main/java/ru/geekbrains/lesson2/Main.java
+++ lesson2/src/main/java/ru/geekbrains/lesson2/Main.java
@@ -1,12 +1,18 @@
 package ru.geekbrains.lesson2;
 
 public class Main {
+    private static final int INF = 1_000_000_000;
+    private static final int NEGATIVE_INF = -INF;
 
     public static void main(String[] args) {
         invertArr();
         fillArr();
         changeArr();
         fillDiagonalElements();
+
+        int[] arr = { -100, -500, 0, 54, 9, 1, -1, -9 };
+        System.out.println("Минимальный элемент массива: " + min(arr));
+        System.out.println("Максимальный элемент массива: " + max(arr));
     }
 
     //Задание №1
@@ -28,7 +34,7 @@ public class Main {
     }
 
     //Задание №3
-    public static void changeArr() {
+    private static void changeArr() {
         int[] arr2 = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
         for (int i = 0; i < arr2.length; i++) {
             if (arr2[i] < 6) {
@@ -39,7 +45,7 @@ public class Main {
     }
 
     //Задание №4
-    public static void fillDiagonalElements() {
+    private static void fillDiagonalElements() {
         int[][] matrix = new int[10][10];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
@@ -48,5 +54,25 @@ public class Main {
             }
             System.out.println();
         }
+    }
+
+    private static int min(int[] arr) {
+        int min = INF;
+        for (int element : arr) {
+            if (element < min) {
+                min = element;
+            }
+        }
+        return min;
+    }
+
+    private static int max(int[] arr) {
+        int max = NEGATIVE_INF;
+        for (int element : arr) {
+            if (element > max) {
+                max = element;
+            }
+        }
+        return max;
     }
 }
