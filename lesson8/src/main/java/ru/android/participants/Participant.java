76a125c5528bbd04375e1c5b9819ef9658b4997e
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-02-21 17:21:26 +0300

    Make Participant to be interface

--- lesson8/src/main/java/ru/android/participants/Participant.java
+++ lesson8/src/main/java/ru/android/participants/Participant.java
@@ -1,66 +1,13 @@
 package ru.android.participants;
 
 import ru.android.obstacles.Obstacle;
-import ru.android.obstacles.Treadmill;
-import ru.android.obstacles.Wall;
 
-public abstract class Participant {
-    protected String name;
-    protected int runningRestriction;
-    protected int jumpingRestriction;
+public interface Participant {
+    void passObstacles(Obstacle[] obstacles);
 
-    public Participant(String name, int runningRestriction, int jumpingRestriction) {
-        this.name = name;
-        this.runningRestriction = runningRestriction;
-        this.jumpingRestriction = jumpingRestriction;
-    }
+    boolean checkDistance(int distance);
+    boolean checkHeight(int height);
 
-    @Override
-    public String toString() {
-        return "Participant{" +
-                "name='" + name + '\'' +
-                ", runningRestriction=" + runningRestriction +
-                ", jumpingRestriction=" + jumpingRestriction +
-                '}';
-    }
-
-    public void passObstacles(Obstacle[] obstacles) {
-        for (Obstacle obstacle : obstacles) {
-            if (obstacle instanceof Treadmill) {
-                int distance = ((Treadmill) obstacle).getDistance();
-
-                if (checkDistance(distance)) {
-                    run(distance);
-                } else {
-                    System.out.printf("Участник %s не сможет пробежать дистанцию в %dм. " +
-                            "Дисквалификация.\n", name, distance);
-                    break;
-                }
-            } else if (obstacle instanceof Wall) {
-                int height = ((Wall) obstacle).getHeight();
-
-                if (checkHeight(height)) {
-                    jump(height);
-                } else {
-                    System.out.printf("Участник %s не сможет перепрыгнуть стену высотой в %dм. " +
-                            "Дисквалификация.\n", name, height);
-                    break;
-                }
-            }
-        }
-
-        System.out.println();
-    }
-
-    private boolean checkDistance(int distance) {
-        return distance <= runningRestriction;
-    }
-
-    private boolean checkHeight(int height) {
-        return height <= jumpingRestriction;
-    }
-
-    public abstract void run(int distance);
-
-    public abstract void jump(int height);
+    void run(int distance);
+    void jump(int height);
 }
--- lesson8/src/main/java/ru/android/participants/Robot.java
+++ lesson8/src/main/java/ru/android/participants/Robot.java
@@ -1,8 +1,56 @@
 package ru.android.participants;
 
-public class Robot extends Participant {
+import ru.android.obstacles.Obstacle;
+import ru.android.obstacles.Treadmill;
+import ru.android.obstacles.Wall;
+
+public class Robot implements Participant {
+    private final String name;
+    private final int runningRestriction; //meters
+    private final int jumpingRestriction; //meters
+
     public Robot(String name, int runningRestriction, int jumpingRestriction) {
-        super(name, runningRestriction, jumpingRestriction);
+        this.name = name;
+        this.runningRestriction = runningRestriction;
+        this.jumpingRestriction = jumpingRestriction;
+    }
+
+    @Override
+    public String toString() {
+        return "Robot{" +
+                "name='" + name + '\'' +
+                ", runningRestriction=" + runningRestriction +
+                ", jumpingRestriction=" + jumpingRestriction +
+                '}';
+    }
+
+    @Override
+    public void passObstacles(Obstacle[] obstacles) {
+        for (Obstacle obstacle : obstacles) {
+            if (obstacle instanceof Treadmill) {
+                int distance = ((Treadmill) obstacle).getDistance();
+
+                if (checkDistance(distance)) {
+                    run(distance);
+                } else {
+                    System.out.printf("Участник %s не сможет пробежать дистанцию в %dм. " +
+                            "Дисквалификация.\n", name, distance);
+                    break;
+                }
+            } else if (obstacle instanceof Wall) {
+                int height = ((Wall) obstacle).getHeight();
+
+                if (checkHeight(height)) {
+                    jump(height);
+                } else {
+                    System.out.printf("Участник %s не сможет перепрыгнуть стену высотой в %dм. " +
+                            "Дисквалификация.\n", name, height);
+                    break;
+                }
+            }
+        }
+
+        System.out.println();
     }
 
     @Override
@@ -13,5 +61,15 @@ public class Robot extends Participant {
     @Override
     public void jump(int height) {
         System.out.printf("Робот %s перепрыгнул стену высотой в %dм. Поздравляю.\n", name, height);
+    }
+
+    @Override
+    public boolean checkDistance(int distance) {
+        return distance <= runningRestriction;
+    }
+
+    @Override
+    public boolean checkHeight(int height) {
+        return height <= jumpingRestriction;
     }
 }
