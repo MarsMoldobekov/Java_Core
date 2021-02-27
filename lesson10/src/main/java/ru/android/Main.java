fcc69c100e6d7fab08baae7e78a92f1d873971d1
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-02-27 23:45:27 +0300

    Write add() and getNumbers() methods

--- lesson10/src/main/java/ru/android/Main.java
+++ lesson10/src/main/java/ru/android/Main.java
@@ -4,11 +4,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    private static final int SIZE = 3;
+
+    private static final String[] lastNames = {
+            "Ivanov", "Petrov", "Karpov"
+    };
+
+    private static final String[][] numbers = {
+            {"+5(099)83-09", "+5(097)87-10", "+5(777)33-39"},
+            {"+999(078)00-09", "+999(078)87-10", "+999(490)44-39"},
+            {"+0-4500-09-45", "+0-4500-10-99", "+0-4900-44-39"}
+    };
 
     public static void main(String[] args) {
         ArrayList<String> array = readFile("./lesson10/src/main/resources/Test.txt");
         System.out.println(deleteDuplicates(array));
         countDuplicates(array);
+
+        Phonebook phonebook = new Phonebook();
+
+        for (int i = 0; i < SIZE; i++) {
+            for (String number : numbers[i]) {
+                phonebook.add(lastNames[i], number);
+            }
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            System.out.println(phonebook.getNumbers(lastNames[i]));
+        }
     }
 
     private static ArrayList<String> readFile(String file) {
--- lesson10/src/main/java/ru/android/Phonebook.java
+++ lesson10/src/main/java/ru/android/Phonebook.java
@@ -1,4 +1,32 @@
 package ru.android;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Phonebook {
+    private final Map<String, List<String>> phonebook;
+
+    public Phonebook() {
+        phonebook = new HashMap<>();
+    }
+
+    public void add(String lastName, String phoneNumber) {
+        if (phonebook.containsKey(lastName)) {
+            phonebook.get(lastName).add(phoneNumber);
+        } else {
+            List<String> numbers = new ArrayList<>();
+            numbers.add(phoneNumber);
+            phonebook.put(lastName, numbers);
+        }
+    }
+
+    public List<String> getNumbers(String lastName) {
+        if (phonebook.containsKey(lastName)) {
+            return phonebook.get(lastName);
+        }
+
+        return null;
+    }
 }
