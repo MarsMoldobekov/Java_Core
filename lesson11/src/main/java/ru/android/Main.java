942637ab6e1f73b3518ea41d2d4551fabbc0946f
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-03-02 18:15:03 +0300

    Create objects of Basket, FruitFactory types

--- lesson11/src/main/java/ru/android/Main.java
+++ lesson11/src/main/java/ru/android/Main.java
@@ -1,15 +1,52 @@
 package ru.android;
 
+import ru.android.basket.Basket;
 import ru.android.factories.FruitFactory;
+import ru.android.fruits.Apple;
+import ru.android.fruits.Orange;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static ru.android.factories.FruitType.*;
+
 public class Main {
     private static final FruitFactory factory = new FruitFactory();
 
-    public static void main(String[] args) {
+    private static final int NUMBER_OF_FRUIT = 10;
 
+    public static void main(String[] args) {
+        // Apple basket
+        Basket<Apple> appleBasket = new Basket<>();
+        ArrayList<Apple> fruitArrayList = new ArrayList<>();
+
+        for (int i = 0; i < NUMBER_OF_FRUIT; i++) {
+            fruitArrayList.add((Apple) factory.getFruit(APPLE));
+        }
+
+        appleBasket.addFruit(fruitArrayList);
+        System.out.println(appleBasket.getBasket());
+
+        // orange basket
+        Basket<Orange> orangeBasket = new Basket<>();
+        ArrayList<Orange> orangeArrayList = new ArrayList<>();
+
+        for (int i = 0; i < NUMBER_OF_FRUIT; i++) {
+            orangeArrayList.add((Orange) factory.getFruit(ORANGE));
+        }
+
+        orangeBasket.addFruit(orangeArrayList);
+        System.out.println(orangeBasket.getBasket());
+
+        // comparing apple and orange baskets
+        System.out.println(appleBasket.compare(orangeBasket));
+
+        // putting fruits from one basket to another
+        Basket<Apple> secondAppleBasket = new Basket<>();
+        secondAppleBasket.putFromBasket(appleBasket);
+
+        System.out.println(appleBasket.getBasket());
+        System.out.println(secondAppleBasket.getBasket());
     }
 
     private static <T> void swap(ArrayList<T> list, int index1, int index2) {
