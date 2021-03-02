68d7ad06ca60ce3cd3e544d7c620ab76e508e52f
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-03-02 14:34:31 +0300

    Check for NPE

--- lesson11/src/main/java/ru/android/Main.java
+++ lesson11/src/main/java/ru/android/Main.java
@@ -1,21 +1,26 @@
 package ru.android;
 
+import ru.android.factories.FruitFactory;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
+    private static final FruitFactory factory = new FruitFactory();
 
     public static void main(String[] args) {
 
     }
 
     private static <T> void swap(ArrayList<T> list, int index1, int index2) {
-        T obj = list.get(index1);
-        list.set(index2, list.get(index1));
-        list.set(index2, obj);
+        if (list != null) {
+            T obj = list.get(index1);
+            list.set(index2, list.get(index1));
+            list.set(index2, obj);
+        }
     }
 
-    private static <T> ArrayList<T> createArrayList(T[] array) {
+    private static <T> ArrayList<T> createArrayList(T[] array) throws NullPointerException {
         return (ArrayList<T>) Arrays.asList(array);
     }
 }
