82c27e48003d20978f7cf1992f9069663b95200f
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-03-02 14:33:22 +0300

    Rename Box.java to Basket.java

---
+++ lesson11/src/main/java/ru/android/box/Basket.java
@@ -0,0 +1,39 @@
+package ru.android.box;
+
+import ru.android.fruits.Fruit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Basket<T extends Fruit> {
+    private final ArrayList<T> basket;
+
+    public Basket() {
+        basket = new ArrayList<>();
+    }
+
+    public List<T> getBasket() {
+        return basket;
+    }
+
+    public void putFromBasket(Basket<? extends T> basket) {
+        if (basket != null) {
+            this.basket.addAll(basket.basket);
+            basket.basket.clear();
+        }
+    }
+
+    public void addFruit(List<? extends T> fruits) {
+        if (fruits != null) {
+            basket.addAll(fruits);
+        }
+    }
+
+    public boolean compare(Basket<?> basket) {
+        return basket != null && getWeight() == basket.getWeight();
+    }
+
+    public float getWeight() {
+        return basket.size() * basket.get(0).getWeight();
+    }
+}
