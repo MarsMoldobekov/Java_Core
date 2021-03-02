ab540af9892d6fd5c82cba110b19c0827b517858
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-03-02 18:11:20 +0300

    Override toString() method

--- lesson11/src/main/java/ru/android/fruits/Apple.java
+++ lesson11/src/main/java/ru/android/fruits/Apple.java
@@ -4,4 +4,11 @@ public class Apple extends Fruit {
     public Apple() {
         weight = 1.0F;
     }
+
+    @Override
+    public String toString() {
+        return "Apple{" +
+                "weight=" + weight +
+                '}';
+    }
 }
--- lesson11/src/main/java/ru/android/fruits/Orange.java
+++ lesson11/src/main/java/ru/android/fruits/Orange.java
@@ -4,4 +4,11 @@ public class Orange extends Fruit {
     public Orange() {
         weight = 1.5F;
     }
+
+    @Override
+    public String toString() {
+        return "Orange{" +
+                "weight=" + weight +
+                '}';
+    }
 }
