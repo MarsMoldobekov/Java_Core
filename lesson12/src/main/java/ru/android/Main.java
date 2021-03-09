7f824344f89bdf1eb60e861b9f9adae7cba9b9fe
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-03-09 17:15:26 +0300

    Write changeElementsByFormula(), move array1 and array2 into anonymous object ref

--- lesson12/src/main/java/ru/android/Main.java
+++ lesson12/src/main/java/ru/android/Main.java
@@ -23,17 +23,17 @@ public class Main {
 
         long a = System.currentTimeMillis();
 
-        for (int i = 0; i < SIZE; i++) {
-            array[i] = (float) (array[i] * sin(0.2f + i / 5.0f) * cos(0.2f + i / 5.0f) * cos(0.4f + i / 2.0f));
-        }
+        array = changeElementsByFormula(array, array.length);
 
         System.out.println(System.currentTimeMillis() - a);
     }
 
     private static void changeArrayAsynchronously() {
         float[] array = new float[SIZE];
-        float[] array1 = new float[HALF_SIZE];
-        float[] array2 = new float[HALF_SIZE];
+        var ref = new Object() {
+            float[] array1 = new float[HALF_SIZE];
+            float[] array2 = new float[HALF_SIZE];
+        };
 
         for (int i = 0; i < SIZE; i++) {
             array[i] = INIT_VALUE;
@@ -41,24 +41,32 @@ public class Main {
 
         long a = System.currentTimeMillis();
 
-        System.arraycopy(array, 0, array1, 0, HALF_SIZE);
-        System.arraycopy(array, HALF_SIZE, array2, 0, HALF_SIZE);
+        System.arraycopy(array, 0, ref.array1, 0, HALF_SIZE);
+        System.arraycopy(array, HALF_SIZE, ref.array2, 0, HALF_SIZE);
 
-        new Thread(() -> {
-            for (int i = 0; i < HALF_SIZE; i++) {
-                array1[i] = (float) (array1[i] * sin(0.2f + i / 5.0f) * cos(0.2f + i / 5.0f) * cos(0.4f + i / 2.0f));
-            }
-        }).start();
+        new Thread(() -> ref.array1 = changeElementsByFormula(ref.array1, ref.array1.length)).start();
+        new Thread(() -> ref.array2 = changeElementsByFormula(ref.array2, ref.array2.length)).start();
 
-        new Thread(() -> {
-            for (int i = 0; i < HALF_SIZE; i++) {
-                array2[i] = (float) (array2[i] * sin(0.2f + i / 5.0f) * cos(0.2f + i / 5.0f) * cos(0.4f + i / 2.0f));
-            }
-        }).start();
-
-        System.arraycopy(array1, 0, array, 0, HALF_SIZE);
-        System.arraycopy(array2, 0, array, HALF_SIZE, HALF_SIZE);
+        System.arraycopy(ref.array1, 0, array, 0, HALF_SIZE);
+        System.arraycopy(ref.array2, 0, array, HALF_SIZE, HALF_SIZE);
 
         System.out.println(System.currentTimeMillis() - a);
+    }
+
+    /**
+     * changeElementsByFormula(array, length)
+     * @param array - массив
+     * @param length - количество элементов в массиве
+     * @return новый массив, элементы которого получаем с помощью формумы:
+     * result[i] = array[i] * sin(0.2f + i / 5.0f) * cos(0.2f + i / 5.0f) * cos(0.4f + i / 2.0f)
+     */
+    private static float[] changeElementsByFormula(float[] array, int length) {
+        float[] result = new float[array.length];
+
+        for (int i = 0; i < length; i++) {
+            result[i] = (float) (array[i] * sin(0.2f + i / 5.0f) * cos(0.2f + i / 5.0f) * cos(0.4f + i / 2.0f));
+        }
+
+        return result;
     }
 }
