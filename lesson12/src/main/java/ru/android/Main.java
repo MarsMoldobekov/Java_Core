8d1fc6a3c987550417ffbdfe911f3ecea42c7fc5
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-03-11 17:30:02 +0300

    Synchronize threads

--- lesson12/src/main/java/ru/android/Main.java
+++ lesson12/src/main/java/ru/android/Main.java
@@ -10,12 +10,14 @@ public class Main {
     private static final float INIT_VALUE = 1.0F;
 
     public static void main(String[] args) {
-        changeArray();
-        changeArrayAsynchronously();
+        Main main = new Main();
+
+        main.changeArray();
+        main.changeArrayAsynchronously();
     }
 
-    private static void changeArray() {
-        float[] array = new float[SIZE];
+    private void changeArray() {
+        final float[] array = new float[SIZE];
 
         for (int i = 0; i < SIZE; i++) {
             array[i] = INIT_VALUE;
@@ -23,17 +25,15 @@ public class Main {
 
         long a = System.currentTimeMillis();
 
-        array = changeElementsByFormula(array, array.length);
+        changeElementsByFormula(array, array.length);
 
         System.out.println(System.currentTimeMillis() - a);
     }
 
-    private static void changeArrayAsynchronously() {
-        float[] array = new float[SIZE];
-        var ref = new Object() {
-            float[] array1 = new float[HALF_SIZE];
-            float[] array2 = new float[HALF_SIZE];
-        };
+    private void changeArrayAsynchronously() {
+        final float[] array = new float[SIZE];
+        final float[] array1 = new float[HALF_SIZE];
+        final float[] array2 = new float[HALF_SIZE];
 
         for (int i = 0; i < SIZE; i++) {
             array[i] = INIT_VALUE;
@@ -41,14 +41,16 @@ public class Main {
 
         long a = System.currentTimeMillis();
 
-        System.arraycopy(array, 0, ref.array1, 0, HALF_SIZE);
-        System.arraycopy(array, HALF_SIZE, ref.array2, 0, HALF_SIZE);
+        System.arraycopy(array, 0, array1, 0, HALF_SIZE);
+        System.arraycopy(array, HALF_SIZE, array2, 0, HALF_SIZE);
 
-        new Thread(() -> ref.array1 = changeElementsByFormula(ref.array1, ref.array1.length)).start();
-        new Thread(() -> ref.array2 = changeElementsByFormula(ref.array2, ref.array2.length)).start();
+        new Thread(() -> changeElementsByFormula(array1, array1.length)).start();
+        new Thread(() -> changeElementsByFormula(array2, array2.length)).start();
 
-        System.arraycopy(ref.array1, 0, array, 0, HALF_SIZE);
-        System.arraycopy(ref.array2, 0, array, HALF_SIZE, HALF_SIZE);
+        synchronized (this) {
+            System.arraycopy(array1, 0, array, 0, HALF_SIZE);
+            System.arraycopy(array2, 0, array, HALF_SIZE, HALF_SIZE);
+        }
 
         System.out.println(System.currentTimeMillis() - a);
     }
@@ -57,16 +59,12 @@ public class Main {
      * changeElementsByFormula(array, length)
      * @param array - массив
      * @param length - количество элементов в массиве
-     * @return новый массив, элементы которого получаем с помощью формумы:
+     * Изменяет элементы массива по формуле:
      * result[i] = array[i] * sin(0.2f + i / 5.0f) * cos(0.2f + i / 5.0f) * cos(0.4f + i / 2.0f)
      */
-    private static float[] changeElementsByFormula(float[] array, int length) {
-        float[] result = new float[array.length];
-
+    private synchronized void changeElementsByFormula(final float[] array, int length) {
         for (int i = 0; i < length; i++) {
-            result[i] = (float) (array[i] * sin(0.2f + i / 5.0f) * cos(0.2f + i / 5.0f) * cos(0.4f + i / 2.0f));
+            array[i] = (float) (array[i] * sin(0.2f + i / 5.0f) * cos(0.2f + i / 5.0f) * cos(0.4f + i / 2.0f));
         }
-
-        return result;
     }
 }
