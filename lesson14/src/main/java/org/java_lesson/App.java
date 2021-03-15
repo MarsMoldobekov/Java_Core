f769087e8fec8d8cc97dc6cd6811fb4987cf75de
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-03-15 21:00:56 +0300

    Create module lesson14

---
+++ lesson14/src/main/java/org/java_lesson/App.java
@@ -0,0 +1,8 @@
+package org.java_lesson;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ lesson14/src/test/java/org/java_lesson/AppTest.java
@@ -0,0 +1,5 @@
+package org.java_lesson;
+
+public class AppTest {
+
+}
