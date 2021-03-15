983c69a988868f09346272eb6755271c1a33592d
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-03-15 20:47:47 +0300

    Rewrite constructor and comment lines in go() method

--- lesson13/src/main/java/ru/android/race/stages/Tunnel.java
+++ lesson13/src/main/java/ru/android/race/stages/Tunnel.java
@@ -3,21 +3,23 @@ package ru.android.race.stages;
 import ru.android.car.Car;
 
 public class Tunnel extends Stage {
-    public Tunnel() {
-        this.length = 80;
+    public Tunnel(int length) {
+        this.length = length;
         this.description = "Тоннель " + length + " метров.";
     }
 
     @Override
     public void go(Car car) {
-        try {
+        /*try {
             System.out.println(car.getName() + " готовится к этапу(ждёт): " + description);
+            semaphore.acquire();
             System.out.println(car.getName() + " начал этап: " + description);
             Thread.sleep(length / car.getSpeed() * 1000L);
         } catch (InterruptedException exception) {
             exception.printStackTrace();
         } finally {
             System.out.println(car.getName() + " закончил этап.");
-        }
+            semaphore.release();
+        }*/
     }
 }
