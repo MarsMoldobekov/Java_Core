e3a61acefd1efeb73b14221abc640b33a921e8d0
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-03-15 20:45:16 +0300

    Implement Builder pattern for Car

--- lesson13/src/main/java/ru/android/car/Car.java
+++ lesson13/src/main/java/ru/android/car/Car.java
@@ -1,11 +1,21 @@
 package ru.android.car;
 
-import ru.android.race.Race;
+import ru.android.concurrent.SingleCountDownLatch;
+import ru.android.concurrent.SingleCyclicBarrier;
+import ru.android.concurrent.SingleSemaphore;
+import ru.android.race.stages.Stage;
+
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
 
 public class Car implements Runnable {
+    private SingleCyclicBarrier cyclicBarrier;
+    private SingleCountDownLatch countDownLatch;
+    private SingleSemaphore semaphore;
+
     private static int CARS_COUNT;
-    private final Race race;
-    private final int speed;
+    private List<Stage> stages;
+    private int speed;
     private final String name;
 
     public int getSpeed() {
@@ -16,9 +26,7 @@ public class Car implements Runnable {
         return name;
     }
 
-    public Car(Race race, int speed) {
-        this.race = race;
-        this.speed = speed;
+    private Car() {
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
@@ -29,8 +37,52 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится.");
             Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов.");
-        } catch (InterruptedException exception) {
+            cyclicBarrier.await();
+        } catch (InterruptedException | BrokenBarrierException exception) {
             exception.printStackTrace();
+        }
+
+        //TODO --- stage.go(this) with the tunnel
+        /*for (Stage stage : stages) {
+            stage.go(this);
+        }*/
+
+        System.out.println(this.name + " завершил гонку.");
+        countDownLatch.countDown();
+    }
+
+    public static Builder getBuilder() {
+        return new Car().new Builder();
+    }
+
+    public class Builder {
+        public Builder setSingleCyclicBarrier(final SingleCyclicBarrier cyclicBarrier) {
+            Car.this.cyclicBarrier = cyclicBarrier;
+            return this;
+        }
+
+        public Builder setSingleCountDownLatch(final SingleCountDownLatch countDownLatch) {
+            Car.this.countDownLatch = countDownLatch;
+            return this;
+        }
+
+        public Builder setSingleSemaphore(final SingleSemaphore semaphore) {
+            Car.this.semaphore = semaphore;
+            return this;
+        }
+
+        public Builder setRaceStages(final List<Stage> stages) {
+            Car.this.stages = stages;
+            return this;
+        }
+
+        public Builder setSpeed(int speed) {
+            Car.this.speed = speed;
+            return this;
+        }
+
+        public Car build() {
+            return Car.this;
         }
     }
 }
