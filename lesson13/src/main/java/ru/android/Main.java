9d21eaa48d3ea46d75ba08f7dd6be44305561646
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-03-16 23:28:00 +0300

    Execute "Race" thread

--- lesson13/src/main/java/ru/android/Main.java
+++ lesson13/src/main/java/ru/android/Main.java
@@ -1,11 +1,9 @@
 package ru.android;
 
 import ru.android.race.Race;
-import ru.android.race.stages.Road;
-import ru.android.race.stages.Tunnel;
 
 public class Main {
     public static void main(String[] args) {
-        new Thread(new Race(new Road(60), new Tunnel(80), new Road(120))).start();
+        new Thread(new Race()).start();
     }
 }
