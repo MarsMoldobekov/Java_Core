7ed35276eb52941c607ae46698be3a9ac74a6d5b
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-03-16 23:34:50 +0300

    Write tests for Array.java

--- lesson14/src/test/java/org/java_lesson/AppTest.java
+++ lesson14/src/test/java/org/java_lesson/AppTest.java
@@ -1,5 +1,47 @@
 package org.java_lesson;
 
-public class AppTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class AppTest {
+    private Array array;
+
+    private static final int[][] intArray = {
+            {1, 1, 4, 4, 1, 4, 4},
+            {1, 1, 1, 1, 1, 1},
+            {4, 4, 4, 4},
+            {1, 4, 4, 1, 1, 4, 3}
+    };
+
+    private static final boolean[] booleanArray = {
+            true, false, false, false
+    };
+
+    @BeforeEach
+    public void init() {
+        array = new Array();
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataForCheckingArr")
+    public void checkArr(boolean result, int[] arr) {
+        Assertions.assertEquals(result, array.checkForOnesAndFours(arr));
+    }
+
+    private static Stream<Arguments> dataForCheckingArr() {
+        List<Arguments> out = new ArrayList<>();
+
+        for (int i = 0; i < booleanArray.length; i++) {
+            out.add(Arguments.arguments(booleanArray[i], intArray[i]));
+        }
+
+        return out.stream();
+    }
 }
